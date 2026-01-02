b44f651e56bef9d6d3dc3d844ba3003d2b619a9b
Abdullah <abdullahkhaled5151@gmail.com>
2026-01-02 05:47:32 +0500

    final backend(jwt)

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './user.service';
 import { UsersController } from './user.controller';
 import { User } from './user.entity';
-
+import { JwtModule } from '@nestjs/jwt';
+  
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), JwtModule.register({
+      secret: 'secretKey123', // dev only
+      signOptions: { expiresIn: '1h' },
+    }),],
   controllers: [UsersController],
   providers: [UsersService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,12 +5,14 @@ import { User } from './user.entity';
 import { RegisterUserDto } from './dto/register-user.dto';
 import * as bcrypt from 'bcrypt';
 import { LoginUserDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    private jwtService: JwtService,
   ) {}
 
  async register(registerDto: RegisterUserDto) {
@@ -51,10 +53,12 @@ async login(loginDto: LoginUserDto) {
     throw new BadRequestException('Invalid credentials');
   }
 
+  const token = this.jwtService.sign({ userId: user.id, email: user.email });
   return {
     message: 'Login successful',
     userId: user.id,
     email: user.email,
+    access_token: token,
   };
 }
 
